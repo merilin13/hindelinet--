d635b8cc3c98bef9b3dee859848f4031b6fa7d51
opilane <150146754+merilin13@users.noreply.github.com>
2024-05-03 13:08:16 +0300

    heu

---
+++ ifandelse/Program.cs
@@ -0,0 +1,32 @@
+﻿namespace ifandelse
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+            int a = 100;
+            int b = 200;
+            
+            /* check the boolean condition */
+            if (a == 100)
+            {
+
+                /* if condition is true then check the following */
+                if (b == 200)
+                {
+                    /* if condition is true then print the following */
+                    Console.WriteLine("väärtus a-l on 100 ja väärtus b-l on 200");
+                }
+            }
+            else
+            {
+                Console.WriteLine("Goodbye, World!");
+            }
+            
+            
+            
+        }
+    }
+    
+}
