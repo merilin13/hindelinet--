caa2530a13ca70c0463e8d48ef21bdc4526f63f5
opilane <150146754+merilin13@users.noreply.github.com>
2024-05-03 12:43:27 +0300

    heyy

---
+++ heh/Program.cs
@@ -0,0 +1,15 @@
+﻿namespace heh
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int length = 8;
+            for (int i = 0; i < length; i++)
+            {
+                Console.WriteLine("WOWWWW");
+
+            }
+        }
+    }
+}
